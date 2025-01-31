2e7610ca757a647c77d5852ab0cff7a8a896d8dc
Richard Goodhue <richard.goodhue@gmail.com>
2025-01-31 09:56:40 -0500

    Updated UserManagement.sol

--- User Management Contract + Test/UserManagement.sol
+++ User Management Contract + Test/UserManagement.sol
@@ -8,32 +8,37 @@ contract UserManagement {
     }
 
     address public owner;
-    mapping(address => User) public users;
+    mapping(address => User) public users; //assigns address to instance of user struct
     uint256 public userCount;
 
     constructor() {
-        owner = msg.sender;
+        owner = msg.sender; //assign owner as contract deployer
     }
 
+    //give only the owner access to certain functions
     modifier onlyOwner() {
-        require(msg.sender == owner, "Sorry, you are not the owner");
+        require(msg.sender == owner, "Sorry, you are not the owner"); 
         _;
     }
 
+    //allows only the owner to add new instance of User struct
     function addUser(address _user, string memory _name, uint256 _balance) public onlyOwner {
         users[_user] = User(_name, _balance);
         userCount++;
     }
 
+    //allows only the owner to change the balance of a specific user that was already added
     function updateUserBalance(address _user, uint256 _newBalance) public onlyOwner {
         users[_user].balance = _newBalance;
     }
 
+    //allows anyone to get info about users added
     function getUserInfo(address _user) public view returns (string memory, uint256) {
         User memory user = users[_user];
         return (user.name, user.balance);
     }
 
+    //allows anyone to return how many users were added in total
     function getTotalUsers() public view returns (uint256) {
         return userCount;
     }
