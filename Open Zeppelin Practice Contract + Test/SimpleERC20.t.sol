be5ddc7f7ed9a574f6170a4ed97499f704847354
Richard Goodhue <richard.goodhue@gmail.com>
2025-02-16 12:54:29 -0500

    SimpleERC20 foundry test added

---
+++ Open Zeppelin Practice Contract + Test/SimpleERC20.t.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "forge-std/Test.sol";
+import "../src/SimpleERC20.sol";
+
+contract SimpleERC20Test is Test {
+    SimpleERC20 token;
+    address owner = address(0x123);
+    address recipient = address(0x456);
+
+    function setUp() public {
+        vm.prank(owner);
+        token = new SimpleERC20(1000 * 10 * 18);
+    }
+
+    function testInitialSupply() public {
+        assertEq(token.totalSupply(), 1000 * 10 * 18);
+        assertEq(token.balanceOf(owner), 1000 * 10 ** 18);
+    }
+
+    function testMint() public {
+        vm.prank(owner);
+        token.mint(recipient, 500 * 10 ** 18);
+        assertEq(token.balanceOf(recipient), 500 * 10 ** 18);
+        assertEq(token.totalSupply(), 1500 * 10 ** 18);
+    }
+
+    function testMintNotOwner() public {
+        vm.prank(recipient);
+        vm.expectRevert("Ownable: caller is not the owner");
+        token.mint(recipient, 500 * 10 ** 18);
+    }
+
+}
