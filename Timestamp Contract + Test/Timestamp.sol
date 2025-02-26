2f294720fb7044f8508f48c9e83afb3203125e84
Richard Goodhue <richard.goodhue@gmail.com>
2025-02-26 15:31:22 -0700

    Timestamp updated

--- Timestamp Contract + Test/Timestamp.sol
+++ Timestamp Contract + Test/Timestamp.sol
@@ -7,10 +7,12 @@ contract TimestampTracker {
         uint256 timestamp;
     }
 
+    //mapping that takes address key and returns associated struct
     mapping(address => Entry) public entries;
 
     event EntryCreated(address indexed user, string data, uint256 timestamp);
 
+    //allows user to add data to struct and tracks timestamp
     function createEntry(string calldata _data) public {
         require(bytes(_data).length > 0, "Data cannot be empty");
         require(entries[msg.sender].timestamp == 0, "Entry already exists");
@@ -23,6 +25,7 @@ contract TimestampTracker {
         emit EntryCreated(msg.sender, _data, block.timestamp);
     }
 
+    //function that allows anyone to check when entrkes were created based off their timestamps
     function getEntryAge(address _user) public view returns (uint256) {
         require(entries[_user].timestamp > 0, "No entry for this user");
 
