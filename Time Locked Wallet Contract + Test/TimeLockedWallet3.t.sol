8b8738f5a38d1cac55a02db0c002b8e8ddcbbdca
Richard Goodhue <richard.goodhue@gmail.com>
2025-03-04 16:17:05 -0500

    TimeLockedWallet3 Foundry Test Added

---
+++ Time Locked Wallet Contract + Test/TimeLockedWallet3.t.sol
@@ -0,0 +1,69 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "forge-std/Test.sol";
+import "../src/TimeLockedWallet3.sol";
+
+contract TimeLockedWalletTest is Test {
+    TimeLockedWallet3 wallet;
+    address owner = address(0x123);
+    address nonOwner = address(0x456);
+    uint256 unlockTime = 1 days;
+
+    function setUp() public {
+        vm.prank(owner);
+        wallet = new TimeLockedWallet3(unlockTime);
+    }
+
+    function testDeposit() public {
+        vm.prank(owner);
+        wallet.deposit{value: 1 ether}();
+
+        assertEq(address(wallet).balance, 1 ether);
+    }
+
+    function testWithdrawal() public {
+        vm.prank(owner);
+        wallet.deposit{value: 1 ether}();
+
+        vm.warp(block.timestamp + unlockTime + 1);
+
+        vm.prank(owner);
+        wallet.withdrawal(1 ether);
+
+        assertEq(address(wallet).balance, 0);
+    }
+
+    function testWithdrawalBeforeUnlock() public {
+        vm.prank(owner);
+        wallet.deposit{value: 1 ether}();
+
+        vm.expectRevert("Sorry your funds are still locked");
+        vm.prank(owner);
+        wallet.withdrawal(1 ether);
+    }
+
+    function testWithdrawalNotOwner() public {
+        vm.prank(owner);
+        wallet.deposit{value: 1 ether}();
+
+        vm.warp(block.timestamp + unlockTime + 1);
+
+        vm.expectRevert("Sorry, you are not the owner");
+        vm.prank(nonOwner);
+        vm.withdrawal(1 ether);
+    }
+
+    function testGetTimeLeft() public {
+        uint256 timeLeft = wallet.getTimeLeft();
+        assertEq(timeLeft, unlockTime);
+
+        vm.warp(block.timestamp + unlockTime / 2);
+        timeLeft - wallet.getTimeLeft();
+        assertEq(timeLeft, unlockTime / 2);
+
+        vm.warp(block.timestamp + unlockTime / 2 + 1);
+        timeLeft = wallet.getTimeLeft();
+        assertEq(timeLeft, 0);
+    }
+}
